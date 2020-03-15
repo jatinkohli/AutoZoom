27daa171f8e205ec58b6570544e685eb4329b158
20jatink <20jatink@students.harker.org>
2020-03-14 23:54:04 -0700

    Fill Queue with times and links from JSON data

---
+++ Main.java
@@ -0,0 +1,111 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Main class
+ * 
+ * @author Jatin Kohli
+ * @since 3/14/20
+ */
+public class Main {
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+    private static Map<String, String> periodLinks;
+    private static Queue<QueueElement> linkQueue;
+
+    /**
+     * Fills periodLinks with Zoom links for each corresponding period key
+     */
+    private static void instantiatePeriodLinks() {
+        periodLinks = new HashMap<String, String>();
+
+        periodLinks.put("P1", "https://harker.zoom.us/wc/join/5408748374");
+        periodLinks.put("P2", "https://harker.zoom.us/wc/join/8699692694");
+        periodLinks.put("P3", "https://harker.zoom.us/wc/join/4328893126");
+        periodLinks.put("P5", "https://harker.zoom.us/wc/join/6614123433");
+        periodLinks.put("P6", "https://harker.zoom.us/wc/join/8904802894");
+        periodLinks.put("P7", "https://harker.zoom.us/wc/join/9655066157");
+        periodLinks.put("Advisory", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
+
+    /**
+     * Fills linkQueue with Zoom links and times
+     */
+    private static void instantiateLinkQueue() {
+        linkQueue = new LinkedList<QueueElement>();
+
+        try {
+            Iterator<Object> scheduleIterator = (new JSONObject(getScheduleJson())).getJSONArray("schedule").iterator();
+
+            while (scheduleIterator.hasNext()) {
+                JSONObject period = (JSONObject)scheduleIterator.next();
+                
+                String time = period.getString("start");
+                String link = periodLinks.get(period.getString("name"));
+
+                if (link != null) 
+                    linkQueue.add(new QueueElement(time, link));
+            }
+        } catch (JSONException e) {
+            System.out.println("No Schedule found for today");
+        }
+    }
+
+    /**
+     * Gets the JSON object representing today's schedule from the HarkerDev API
+     */
+    private static String getScheduleJson() {
+        LocalDate date = LocalDate.now();
+
+        String getScheduleCommand = String.format(
+            "curl --request GET --url https://bell.dev.harker.org/api/schedule --header \"Content-Type: application/x-www-form-urlencoded\" --data month=%s --data day=%s --data year=%s",
+            date.getMonthValue(), date.getDayOfMonth() + 2, date.getYear()
+        );
+
+        String schedule = null;
+
+        try {
+            Process process = (new ProcessBuilder()).command("cmd.exe", "/c", getScheduleCommand).start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            schedule = reader.readLine();
+
+            process.waitFor();
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return schedule;
+    }
+
+    /**
+     * Main method
+     */
+    public static void main(String[] args) {
+        instantiatePeriodLinks();
+        instantiateLinkQueue();
+
+        while (!linkQueue.isEmpty()) {
+            QueueElement e = linkQueue.remove();
+
+            LocalDateTime time = LocalDateTime.parse(e.time, TIME_FORMATTER);
+            System.out.println(time.getHour());
+            System.out.println(time.getMinute());
+            System.out.println(time.getSecond());
+            System.out.println();
+        }
+    }
+}
